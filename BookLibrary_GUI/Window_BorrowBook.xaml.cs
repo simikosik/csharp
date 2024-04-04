c6677af95a72210c15e742f6d501ab164bb59e62
simikosik <kosiksimi5@gmail.com>
2024-04-04 09:11:59 +0200

    wpf app 7 4.4

--- BookLibrary_GUI/Window_BorrowBook.xaml.cs
+++ BookLibrary_GUI/Window_BorrowBook.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,14 +18,33 @@ namespace BookLibrary_GUI
     /// <summary>
     /// Interaction logic for Window_BorrowBook.xaml
     /// </summary>
+   
     public partial class Window_BorrowBook : Window
     {
+        public Library LibraryKNM { get; set; } = Library.Instance;
         public Window_BorrowBook()
         {
             InitializeComponent();
+
+            Initialize();
         }
 
-        private void ListView_SelectBooks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void Initialize()
+        {
+            
+            ListView_SelectBooks.Items.Clear();
+            foreach (var book in LibraryKNM.Books.Where(x => !x.IsBorrowed).ToList())
+            {
+                ListView_SelectBooks.Items.Add(book.ToString());
+            }
+            ListView_SelectUsers.Items.Clear();
+            foreach (var user in LibraryKNM.Users)
+            {
+                ListView_SelectUsers.Items.Add(user.ToString());
+            }
+        }
+
+            private void ListView_SelectBooks_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
@@ -33,4 +53,18 @@ namespace BookLibrary_GUI
         {
 
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var title = ListView_SelectBooks.SelectedItem.ToString();
+            var name = ListView_SelectUsers.SelectedItem.ToString();
+            
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
+}
