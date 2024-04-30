3bd01046ce3e1ca99d1ce5d38e2019a7263ae2e4
simikosik <kosiksimi5@gmail.com>
2024-04-30 10:06:57 +0200

    DC wpf project 1

---
+++ DC/WindowCreditScreen.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DC
+{
+    /// <summary>
+    /// Interaction logic for WindowCreditScreen.xaml
+    /// </summary>
+    public partial class WindowCreditScreen : Window
+    {
+        public WindowCreditScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
