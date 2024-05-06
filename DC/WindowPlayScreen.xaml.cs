6f2751c4b999acb385f8541852000005615019a6
simikosik <kosiksimi5@gmail.com>
2024-05-06 22:35:11 +0200

    wpf counter

--- DC/WindowPlayScreen.xaml.cs
+++ DC/WindowPlayScreen.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,18 @@ namespace DC
     /// </summary>
     public partial class WindowPlayScreen : Window
     {
+        private int counter = 0;
+        
         public WindowPlayScreen()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            counter++;
+
+            CounterLabel.Content = "Ďugcookies: " + counter.ToString();
         }
     }
 }
